6c96ce16cf17c112a14387cc5155fe076ccb7985
Evgen <kurakin.evgeny@gmail.com>
2021-09-15 17:23:58 +0300

    Создал компонент TableBody

--- src/components/userTable.jsx
+++ src/components/userTable.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import User from './user'
+// import User from './user'
 import TableHeader from './tableHeader'
+import TableBody from './tableBody'
 
 const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
   const columns = {
-    name: { iter: 'name', name: 'Имя' },
+    name: { path: 'name', name: 'Имя' },
     quolities: { name: 'Качества' },
-    professions: { iter: 'profession.name', name: 'Профессия' },
-    complitedMeetings: { iter: 'completedMeetings', name: 'Встретился, раз' },
-    rate: { iter: 'rate', name: 'Оценка' },
-    bookmark: { iter: 'bookmark', name: 'Избранное' },
+    professions: { path: 'profession.name', name: 'Профессия' },
+    complitedMeetings: { path: 'completedMeetings', name: 'Встретился, раз' },
+    rate: { path: 'rate', name: 'Оценка' },
+    bookmark: { path: 'bookmark', name: 'Избранное' },
     delete: {}
   }
 
   return (
     <table className="table">
       <TableHeader {...{ onSort, selectedSort, columns }} />
-      <tbody>
+      <TableBody {...{ columns, data: users }} />
+      {/* <tbody>
         {users.map((user) => (
           <User key={user._id} user={user} {...rest} />
         ))}
-      </tbody>
+      </tbody> */}
     </table>
   )
 }
