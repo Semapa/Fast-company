3af77423a7bb62e14f5e0cf717367fdb8e65ddfb
Evgen <kurakin.evgeny@gmail.com>
2021-09-15 15:00:36 +0300

    Рефакторинг кода и создание компонента userTable

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import User from './user'
 import Pagination from './pagination'
 import GroupList from './groupList'
 import SearchStatus from './searchStatus'
+import UserTable from './userTable'
 import { paginate } from '../utils/paginate'
 import api from '../api/index'
 
@@ -83,26 +83,7 @@ const Users = ({ users: allUsers, ...rest }) => {
       )}
       <div className="d-flex flex-column">
         <SearchStatus length={count} />
-        {count > 0 && (
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Избранное</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <User key={user._id} user={user} {...rest} />
-              ))}
-            </tbody>
-          </table>
-        )}
+        {count > 0 && <UserTable users={users} {...rest} />}
         <div className="d-flex justify-content-center">
           <Pagination
             itemCount={count}
