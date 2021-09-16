3313c275b5e6a67ed86aace366b29f9c0b0e7d06
Evgen <kurakin.evgeny@gmail.com>
2021-09-16 15:38:18 +0300

    Рефакторинг компонента users

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Users from './components/users'
-import Loader from './components/UI/Loader/loader'
-import api from './api/index'
 
 function App() {
-  const [users, setUsers] = useState()
-  const [loader, setLoader] = useState(true)
-
-  useEffect(() => {
-    api.users.fetchAll().then((data) => {
-      setUsers(data)
-      setLoader(false)
-    })
-  }, [])
-
-  const handleDelete = (userId) => {
-    setUsers(users.filter((user) => userId !== user._id))
-  }
-
-  const handleToggleBookMark = (id) => {
-    setUsers(
-      users.filter((user) => {
-        if (user._id === id) {
-          user.bookmark = !user.bookmark
-          return user
-        }
-        return user
-      })
-    )
-  }
-
-  return (
-    <>
-      {loader ? (
-        <Loader />
-      ) : (
-        <Users
-          users={users}
-          onDelete={handleDelete}
-          onToggleBookMark={handleToggleBookMark}
-        />
-      )}
-    </>
-  )
+  return <Users />
 }
 
 export default App
