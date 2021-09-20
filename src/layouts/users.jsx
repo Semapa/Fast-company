b9a8f915ad6243efe7cf9159c8beb7b1ec446f12
Evgen <kurakin.evgeny@gmail.com>
2021-09-20 18:56:21 +0300

    Создал новые компоненты для организации роутинга

---
+++ src/layouts/users.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/navBar'
+import Users from '../components/users'
+
+const LayoutUsers = () => {
+  return (
+    <>
+      <NavBar />
+      <Users />
+    </>
+  )
+}
+
+export default LayoutUsers
