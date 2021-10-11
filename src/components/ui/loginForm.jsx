2a54afba2d39dadc9d2cb42b62830ffa10ae2102
Evgen <kurakin.evgeny@gmail.com>
2021-10-11 15:14:11 +0300

    Добавил валидацию с помощью Yup

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,45 +1,74 @@
 import React, { useEffect, useState } from 'react'
-import { validator } from '../../utils/validator'
+// import { validator } from '../../utils/validator'
 import TextField from '../common/form/textField'
 import CheckBoxField from '../common/form/checkBoxField'
+import * as yup from 'yup'
 
 const LoginForm = () => {
   const [data, setData] = useState({ email: '', password: '', stayOn: false })
   const [errors, setErrors] = useState({})
 
-  const validatorConfig = {
-    email: {
-      isRequired: {
-        message: 'Электронная почта обязательна для заполнения'
-      },
-      isEmail: {
-        message: 'Email введен не корректно'
-      }
-    },
-    password: {
-      isRequired: {
-        message: 'Пароль обязателен для заполнения'
-      },
-      isCapitalSymbol: {
-        message: 'Пароль должен содержать хотя бы одну заглавную букву'
-      },
-      isContainDigit: {
-        message: 'Пароль должен содержать хотя бы одну цифру'
-      },
-      min: {
-        message: 'Пароль должен состоять минимум из 8 символов',
-        value: 8
-      }
-    }
-  }
+  const validateScheme = yup.object().shape({
+    password: yup
+      .string()
+      .required('Пароль обязателен для заполнения')
+      .matches(
+        /^(?=.*[A-Z])/,
+        'Пароль должен содержать хотя бы одну заглавную букву'
+      )
+      .matches(/(?=.*[0-9])/, 'Пароль должен содержать хотя бы одну цифру')
+      .matches(
+        /(?=.*[!@#$%^&*])/,
+        'Пароль должен содержать один из специальных символов !@#$%^&*'
+      )
+      .matches(/(?=.{8,})/, 'Пароль должен состоять минимум из 8 символов'),
+    email: yup
+      .string()
+      .required('Электронная почта обязательна для заполнения')
+      .email('Email введен не корректно')
+  })
+
+  // валидация без Yup
+  // const validatorConfig = {
+  //   email: {
+  //     isRequired: {
+  //       message: 'Электронная почта обязательна для заполнения'
+  //     },
+  //     isEmail: {
+  //       message: 'Email введен не корректно'
+  //     }
+  //   },
+  //   password: {
+  //     isRequired: {
+  //       message: 'Пароль обязателен для заполнения'
+  //     },
+  //     isCapitalSymbol: {
+  //       message: 'Пароль должен содержать хотя бы одну заглавную букву'
+  //     },
+  //     isContainDigit: {
+  //       message: 'Пароль должен содержать хотя бы одну цифру'
+  //     },
+  //     min: {
+  //       message: 'Пароль должен состоять минимум из 8 символов',
+  //       value: 8
+  //     }
+  //   }
+  // }
 
   useEffect(() => {
     validate()
   }, [data])
 
   const validate = () => {
-    const errors = validator(data, validatorConfig)
-    setErrors(errors)
+    // валидация без Yup
+    // const errors = validator(data, validatorConfig)
+
+    validateScheme
+      .validate(data)
+      .then(() => setErrors({}))
+      .catch((err) => setErrors({ [err.path]: err.message }))
+    // валидация без Yup
+    // setErrors(errors)
     return Object.keys(errors).length === 0
   }
   const isValid = Object.keys(errors).length === 0
