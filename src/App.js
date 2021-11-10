6be9400beb13b69b4a928adf815cbaca7a559560
Evgen <kurakin.evgeny@gmail.com>
2021-11-10 16:49:18 +0300

    Добавил Хук useProfessions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router'
 import { ToastContainer } from 'react-toastify'
 import NavBar from './components/ui/navBar'
+import { ProfessionProvider } from './hooks/useProfession'
 import Login from './layouts/login'
 import LayoutMain from './layouts/main'
 import LayoutUsers from './layouts/users'
@@ -11,8 +12,10 @@ function App() {
     <div>
       <NavBar />
       <Switch>
-        <Route path="/users/:userId?/:edit?" component={LayoutUsers} />
-        <Route path="/login/:type?" component={Login} />
+        <ProfessionProvider>
+          <Route path="/users/:userId?/:edit?" component={LayoutUsers} />
+          <Route path="/login/:type?" component={Login} />
+        </ProfessionProvider>
         <Route path="/" exact component={LayoutMain} />
       </Switch>
       <ToastContainer />
---
+++ src/hooks/useProfession.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { toast } from 'react-toastify'
+import PropTypes from 'prop-types'
+import professionService from '../services/profession.service'
+
+const ProfessionContext = React.createContext()
+
+export const useProfessions = () => {
+  return useContext(ProfessionContext)
+}
+
+export const ProfessionProvider = ({ children }) => {
+  const [isLoading, setLoading] = useState(true)
+  const [profession, setProfession] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getProfessionsList()
+  }, [])
+
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error)
+      setError(null)
+    }
+  }, [error])
+
+  function errorCatcher(error) {
+    const { message } = error.response.data
+    setError(message)
+    setLoading(false)
+  }
+
+  function getProfession(id) {
+    return profession.find((p) => p._id === id)
+  }
+
+  async function getProfessionsList() {
+    try {
+      const { content } = await professionService.get()
+      setProfession(content)
+      setLoading(false)
+    } catch (error) {
+      errorCatcher(error)
+    }
+  }
+
+  return (
+    <ProfessionContext.Provider
+      value={{ profession, isLoading, getProfession }}
+    >
+      {children}
+    </ProfessionContext.Provider>
+  )
+}
+
+ProfessionProvider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+}
