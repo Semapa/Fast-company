48768336d4d70e2a44282b32add214704b062e8b
Evgen <kurakin.evgeny@gmail.com>
2021-11-17 12:24:55 +0300

    Добавил трансформацию данных в http.service

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,14 +1,52 @@
 import axios from 'axios'
 import { toast } from 'react-toastify'
-import config from '../config.json'
+import configFile from '../config.json'
 
 // При такой настройке можно убрать основную часть url
 // из методов .get, .put и тд (н.п. editQuality.jsx)
-axios.defaults.baseURL = config.apiEndpoint
+axios.defaults.baseURL = configFile.apiEndpoint
+
+// Чтобы не менять данные по эндпойнтам, перехватываем здесь
+// изменяем на нужные нам нп profession/ на profession.json
+// или profession/34234 на profession/34234.json
+axios.interceptors.request.use(
+  function (config) {
+    // Если используем Firebase? то меняем эндпойнты
+    if (configFile.isFireBase) {
+      // Провверяем есть ли в конце "/"
+      const containSlash = /\/$/gi.test(config.url)
+      config.url =
+        (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
+      console.log('config.url', config.url)
+    }
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+
+function transformData(data) {
+  // Проверяем существует ли data, т.к. Firebase в случае ошибки вернет null
+  return data
+    ? Object.keys(data).map((key) => ({
+        // в этом месте если необходимо можно добавить id, в нашем случае он есть внутри объекта
+        ...data[key]
+      }))
+    : []
+}
 
 // Перехватчики ошибок при работе с сервером
 axios.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    // если это FireBase
+    if (configFile.isFireBase) {
+      // т.к. в хуке useUsers используем {content} необходимо трансформировать res.data
+      res.data = { content: transformData(res.data) }
+      console.log(res.data)
+    }
+    return res
+  },
   function (error) {
     const expectedErrors =
       error.response &&
