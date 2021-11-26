48cfb29143676cbed3e482b370b783a28ceb7a8f
Evgen <kurakin.evgeny@gmail.com>
2021-11-26 17:05:20 +0300

    Добавил LocalStorageService

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { toast } from 'react-toastify'
 import axios from 'axios'
 import userService from '../services/user.service'
+import { setTokens } from '../services/localStorage.service'
 
 const httpAuth = axios.create()
 const AuthContext = React.createContext()
@@ -11,26 +12,12 @@ export const useAuth = () => {
   return useContext(AuthContext)
 }
 
-const TOKEN_KEY = 'jwt-token'
-const REFRESH_KEY = 'jwt-refresh-token'
-const EXPIRES_KEY = 'jwt-expires'
-
 const AuthProvider = ({ children }) => {
   const [currentUser, setUser] = useState({})
   const [error, setError] = useState(null)
 
-  function setToken({ refreshToken, idToken, expiresIn = 3600 }) {
-    // Трансформируем expiresIn из секунд в миллисекунды
-    // и получаем TimeStamp времени в которое токен истечет
-    const expiresDate = new Date().getTime() + expiresIn * 1000
-    localStorage.setItem(TOKEN_KEY, idToken)
-    localStorage.setItem(REFRESH_KEY, refreshToken)
-    localStorage.setItem(EXPIRES_KEY, expiresDate)
-  }
-
   async function signUp({ email, password, ...rest }) {
-    const key = 'AIzaSyANciJd9cw0FecQQnj3CV_vVU6-ENwqwDQ'
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`
     try {
       // Используем обычный axios, а не наш http.service т.к. в там много различных исключений
       const { data } = await httpAuth.post(url, {
@@ -39,7 +26,7 @@ const AuthProvider = ({ children }) => {
         returnSecureToken: true
       })
       console.log('useAuth data', data)
-      setToken(data)
+      setTokens(data)
       await createUser({ _id: data.localId, email, ...rest })
     } catch (error) {
       errorCatcher(error)
