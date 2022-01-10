7e7b69e4f64bc6093862d10542031bba53eabc6c
Evgen <kurakin.evgeny@gmail.com>
2022-01-10 17:03:32 +0300

    Реализовал компонент navProfile

---
+++ src/components/ui/navProfile.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
+import Avatar from '../common/avatar'
+
+const NavProfile = () => {
+  const { currentUser } = useAuth()
+  const [isOpen, setOpen] = useState(false)
+  const toggleMenu = () => {
+    setOpen((prevState) => !prevState)
+  }
+  return (
+    <div className="dropdown" onClick={toggleMenu}>
+      <div className="btn dropdown-toggle d-flex align-items-center">
+        <div className="me-2">{currentUser.name}</div>
+        <Avatar classes="rounded-circle" width="40" height="40" />
+      </div>
+      <div className={'w-100 dropdown-menu' + (isOpen ? ' show' : '')}>
+        <Link to={`/users/${currentUser._id}`} className="dropdown-item">
+          Profile
+        </Link>
+        <Link to="logout" className="dropdown-item">
+          Log Out
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NavProfile
