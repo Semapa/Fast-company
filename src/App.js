9581c9d7e837a8be61a4e51f0828e1e6e3cd1eeb
Evgen <kurakin.evgeny@gmail.com>
2022-01-10 17:47:36 +0300

    Реализовал компонент protectesRoute

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import ProtectedRoute from './components/common/protectedRoute'
 import NavBar from './components/ui/navBar'
 import AuthProvider from './hooks/useAuth'
 import { ProfessionProvider } from './hooks/useProfession'
@@ -18,7 +19,10 @@ function App() {
         <QualitiesProvider>
           <ProfessionProvider>
             <Switch>
-              <Route path="/users/:userId?/:edit?" component={LayoutUsers} />
+              <ProtectedRoute
+                path="/users/:userId?/:edit?"
+                component={LayoutUsers}
+              />
               <Route path="/login/:type?" component={Login} />
 
               <Route path="/" exact component={LayoutMain} />
