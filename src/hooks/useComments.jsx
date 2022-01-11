7e024ff7b6cfe13a92f0ac993a08d6d2f5d2cee0
Evgen <kurakin.evgeny@gmail.com>
2022-01-11 15:30:06 +0300

    Добавил запрос для добавления комметнария в Firebase

--- src/hooks/useComments.jsx
+++ src/hooks/useComments.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useContext } from 'react'
-// import { toast } from 'react-toastify'
+import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import { useParams } from 'react-router'
 import { useAuth } from './useAuth'
 import { nanoid } from 'nanoid'
+import commentService from '../services/comment.service'
 
 const CommenstContext = React.createContext()
 
@@ -14,12 +15,13 @@ export const useComments = () => {
 export const CommentsProvider = ({ children }) => {
   const { userId } = useParams()
   const { currentUser } = useAuth()
-  //   const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true)
   const [comments, setComments] = useState([])
-  //   const [error, setError] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     setComments(null)
+    setLoading(false)
   }, [])
 
   async function createComment(data) {
@@ -31,10 +33,29 @@ export const CommentsProvider = ({ children }) => {
       userId: currentUser._id
     }
     console.log('useComments comment', comment)
+    try {
+      const { content } = await commentService.createComment(comment)
+      console.log('useComments content', content)
+    } catch (error) {
+      errorCatcher(error)
+    }
   }
 
+  function errorCatcher(error) {
+    // console.log('e', error)
+    const { message } = error.response.data
+    setError(message)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error)
+      setError(null)
+    }
+  }, [error])
   return (
-    <CommenstContext.Provider value={{ comments, createComment }}>
+    <CommenstContext.Provider value={{ comments, createComment, isLoading }}>
       {children}
     </CommenstContext.Provider>
   )
