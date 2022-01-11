1c282857b9a558d24a8b4417521a26c2f045033d
Evgen <kurakin.evgeny@gmail.com>
2022-01-11 15:17:06 +0300

    Создал useComments

--- src/hooks/useComments.jsx
+++ src/hooks/useComments.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
 // import { toast } from 'react-toastify'
 import PropTypes from 'prop-types'
+import { useParams } from 'react-router'
+import { useAuth } from './useAuth'
+import { nanoid } from 'nanoid'
 
 const CommenstContext = React.createContext()
 
@@ -9,6 +12,8 @@ export const useComments = () => {
 }
 
 export const CommentsProvider = ({ children }) => {
+  const { userId } = useParams()
+  const { currentUser } = useAuth()
   //   const [isLoading, setLoading] = useState(true)
   const [comments, setComments] = useState([])
   //   const [error, setError] = useState(null)
@@ -17,8 +22,19 @@ export const CommentsProvider = ({ children }) => {
     setComments(null)
   }, [])
 
+  async function createComment(data) {
+    const comment = {
+      ...data,
+      _id: nanoid(),
+      pageId: userId,
+      created_at: Date.now(),
+      userId: currentUser._id
+    }
+    console.log('useComments comment', comment)
+  }
+
   return (
-    <CommenstContext.Provider value={{ comments }}>
+    <CommenstContext.Provider value={{ comments, createComment }}>
       {children}
     </CommenstContext.Provider>
   )
