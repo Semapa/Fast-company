290e502381c19442088fb8e8b76b8bcafd6aa875
Evgen <kurakin.evgeny@gmail.com>
2022-01-21 10:27:46 +0300

    Создал store

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from './store/createSrore'
+import { Provider } from 'react-redux'
+
+const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
