ecb1def4484ab418635a53f608bbfb0097917792
Evgen <kurakin.evgeny@gmail.com>
2022-02-07 14:20:14 +0300

    implemented get qualities, professions

--- server/routes/profession.routes.js
+++ server/routes/profession.routes.js
@@ -1,4 +1,23 @@
 const express = require('express')
+const Profession = require('../models/Profession')
 const router = express.Router({ mergeParams: true })
+
+router.get('/', async (req, res) => {
+  try {
+    // const professions = await Profession.find()
+    // res.status(200).json({
+    //   list: professions
+    // })
+    // более короткая запись
+    const list = await Profession.find()
+    // res.status(200).json({ list }) при такой записи возвращается ключ list: и за ним уже массив с провфессиями
+    res.status(200).send(list) // нам ключ лист не нужен, поэтому делаем таким способом
+  } catch (error) {
+    // отправится на клиент
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже...'
+    })
+  }
+})
 
 module.exports = router
--- server/routes/quality.routes.js
+++ server/routes/quality.routes.js
@@ -1,4 +1,17 @@
 const express = require('express')
+const Quality = require('../models/Quality')
 const router = express.Router({ mergeParams: true })
+
+router.get('/', async (req, res) => {
+  try {
+    const list = await Quality.find()
+    res.status(200).send(list) // нам ключ лист не нужен, поэтому вместо json() делаем таким способом
+  } catch (error) {
+    // отправится на клиент
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже...'
+    })
+  }
+})
 
 module.exports = router
