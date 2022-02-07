d4ace3a8aa60ff0fa82a656a609893d25620dde3
Evgen <kurakin.evgeny@gmail.com>
2022-02-07 15:46:34 +0300

    Рефакторинг маршрутов

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 
+router.get('/', async (req, res) => {})
+
+router.post('/', async (req, res) => {})
+
+router.post('/:id', async (req, res) => {})
+
 module.exports = router
