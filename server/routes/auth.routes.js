06a86a23a65b2bb1a00bb092656d02ca98b3d521
Evgen <kurakin.evgeny@gmail.com>
2022-02-07 17:58:38 +0300

    Добавлена валидация полей в auth.route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const { generateUserData } = require('../utils/helpers')
 const tokenService = require('../services/token.service')
@@ -12,47 +13,67 @@ const router = express.Router({ mergeParams: true })
 // 4. Создать пользователя
 // 5. Сгенерировать токены
 
-router.post('/signUp', async (req, res) => {
-  try {
-    // req.body содержатся данные, которые отправляются методом post
-    const { email, password } = req.body
+router.post('/signUp', [
+  // перечисляем правила для валидации
+  check('email', 'Некорректный email').isEmail(),
+  check('password', 'Минимальная длинна пароля 8 символов').isLength({
+    min: 8
+  }),
+  async (req, res) => {
+    try {
+      // Проверяем валидации
+      const errors = validationResult(req)
+      // Если есть ошибки
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_DATA',
+            code: 400
+            // errors: errors.array()  // для обработки ошибок, прилетает подробный массив
+          }
+        })
+      }
 
-    // В коллекции пользователей ищем пользователя по email
-    const exitingUser = await User.findOne({ email })
+      // req.body содержатся данные, которые отправляются методом post
+      const { email, password } = req.body
 
-    // Если уже есть такой пользователь
-    if (exitingUser) {
-      return res.status(400).json({
-        // эмулируем формат ошибок из firebase
-        error: {
-          message: 'EMAIL_EXISTS',
-          code: 400
-        }
+      // В коллекции пользователей ищем пользователя по email
+      const exitingUser = await User.findOne({ email })
+
+      // Если уже есть такой пользователь
+      if (exitingUser) {
+        return res.status(400).json({
+          // эмулируем формат ошибок из firebase
+          error: {
+            message: 'EMAIL_EXISTS',
+            code: 400
+          }
+        })
+      }
+
+      // Шифруем пароль (12 - сложность шифрования)
+      const hashedPassword = await bcrypt.hash(password, 12)
+
+      // Создаем пользователя в БД
+      const newUser = await User.create({
+        // Ставим на первое место, тк в последствии эти данные могут приходить в req.body
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword
+      })
+
+      // Генерируем набор токенов
+      const tokens = tokenService.generate({ _id: newUser._id })
+      await tokenService.save(newUser._id, tokens.refreshToken)
+
+      res.status(201).send({ ...tokens, userId: newUser._id })
+    } catch (error) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже...'
       })
     }
-
-    // Шифруем пароль (12 - сложность шифрования)
-    const hashedPassword = await bcrypt.hash(password, 12)
-
-    // Создаем пользователя в БД
-    const newUser = await User.create({
-      // Ставим на первое место, тк в последствии эти данные могут приходить в req.body
-      ...generateUserData(),
-      ...req.body,
-      password: hashedPassword
-    })
-
-    // Генерируем набор токенов
-    const tokens = tokenService.generate({ _id: newUser._id })
-    await tokenService.save(newUser._id, tokens.refreshToken)
-
-    res.status(201).send({ ...tokens, userId: newUser._id })
-  } catch (error) {
-    res.status(500).json({
-      message: 'На сервере произошла ошибка. Попробуйте позже...'
-    })
   }
-})
+])
 
 router.post('/signInWithPassword', async (req, res) => {})
 
